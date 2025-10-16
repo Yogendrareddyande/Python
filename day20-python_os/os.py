29ade8e67756375ae2b981777cfcb70ffcd92c45
Yogendrareddyande <andeyogendra25@gmail.com>
2025-10-17 00:58:16 +0530

    DAY-20

--- day20-python_os/os.py
+++ day20-python_os/os.py
@@ -9,9 +9,51 @@
 
 
 import os
+import time
 
 '''
 c:\\users\\user\\oneDrive\\Desktop\\
 '''
 
+print(os.path.exists('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os'))
+
+print(os.path.isdir('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os'))
+
+print(os.path.dirname('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py'))
+print(os.path.basename('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py'))
+
+print(time.time())
+print(time.ctime(time.time()))
+t=os.path.getctime('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py')
+print(time.ctime(t))
+
+
+
+print(os.name)
+print(os.getcwd())
+
+os.chdir('c:\\Users\\Yogi\\OneDrive\\Desktop')
+print(os.getcwd())
+
+# os.mkdir('TEST')
+
+os.chdir('c:\\Users\\Yogi\\OneDrive\\Desktop\\TEST')
+# f=open('test.txt','w')
+# f.write('''
+# hi hello
+#         how are you
+
+# ''')
+# f.close()
+
+# with open('test.txt','r') as re:
+#     print(re.read())
+
+# os.remove('test.txt')
+
+# os.makedirs('Grandparent/parent/child')
+# os.removedirs('Grandparent/parent/child')
+
+# os.mkdir('TEST')
+os.rename('TEST','DEMO')
 
