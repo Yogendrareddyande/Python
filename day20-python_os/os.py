d4de3e90162b7064cb1fead306be28ae1f0f6423
Yogendrareddyande <andeyogendra25@gmail.com>
2025-10-17 00:17:19 +0530

    Python

---
+++ day20-python_os/os.py
@@ -0,0 +1,17 @@
+# with open ('test.txt','w') as f:
+#     f.write(''' 
+#               Hi hello how are you
+#             i am doing grate!
+#            ''')
+# with open('test.txt','r') as f:
+#     text=f.read()
+#     print(text)
+
+
+import os
+
+'''
+c:\\users\\user\\oneDrive\\Desktop\\
+'''
+
+
