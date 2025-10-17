2a3c103cdb8bacb5087ed90c6bef459f6bb7e05c
Yogendrareddyande <andeyogendra25@gmail.com>
2025-10-17 19:29:04 +0530

    python

--- day21-csv_file/csv.py
+++ day21-csv_file/csv.py
@@ -1,4 +1,21 @@
-# CSV stands for Comma-Separated Values.
+# CSV stands for Comma-Separated Values,
+
+# import csv
+
+# import os
 
 
 
+# os.chdir('\\Users\\yogi\\OneDrive\\Desktop\\python')
+# print(os.getcwd())
+
+import csv
+import os
+
+# Always use the absolute path to the file
+
+
+with open('EMp', 'r') as f:
+    csv_reader = csv.reader(f)
+    for row in csv_reader:
+        print(row)
---
+++ day22-exception/ex.py
@@ -0,0 +1,40 @@
+# a=int(input('Enter first number:'))
+# b=int(input('Enter second number:'))
+
+# if a>b:
+#     print(a)
+# else:
+#     print(a)
+
+# a=int(input('Enter a number:'))
+# if a>10:
+#     if a%2==0:
+#         print('a is even number')
+#     else:
+#         print('a is not even number')
+
+# else:
+#     print('Enter a valid number')
+
+# a=10
+# b='yogi'
+
+# try:
+#     a//b
+# except (ZeroDivisionError,TypeError):
+#     print('A number can not be divisible by zero')
+# finally:
+#     print('program completed')
+
+a=10
+b=2
+
+try:
+    res=a//b
+except Exception as msg:
+    print(msg)
+else:
+    print(res)
+finally:
+    print('program completed')
+
