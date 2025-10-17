aeec788aa5816d07abbdb0047d4ca4bff3b6b257
Yogendrareddyande <andeyogendra25@gmail.com>
2025-10-17 10:29:11 +0530

    updated

--- day20-python_os/os.py
+++ day20-python_os/os.py
@@ -8,36 +8,36 @@
 #     print(text)
 
 
-import os
-import time
+# import os
+# import time
 
-'''
-c:\\users\\user\\oneDrive\\Desktop\\
-'''
+# '''
+# c:\\users\\user\\oneDrive\\Desktop\\
+# '''
 
-print(os.path.exists('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os'))
+# print(os.path.exists('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os'))
 
-print(os.path.isdir('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os'))
+# print(os.path.isdir('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os'))
 
-print(os.path.dirname('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py'))
-print(os.path.basename('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py'))
+# print(os.path.dirname('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py'))
+# print(os.path.basename('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py'))
 
-print(time.time())
-print(time.ctime(time.time()))
-t=os.path.getctime('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py')
-print(time.ctime(t))
+# print(time.time())
+# print(time.ctime(time.time()))
+# t=os.path.getctime('c:\\Users\\Yogi\\OneDrive\\Desktop\\Python\\day20-python_os/os.py')
+# print(time.ctime(t))
 
 
 
-print(os.name)
-print(os.getcwd())
+# print(os.name)
+# print(os.getcwd())
 
-os.chdir('c:\\Users\\Yogi\\OneDrive\\Desktop')
-print(os.getcwd())
+# os.chdir('c:\\Users\\Yogi\\OneDrive\\Desktop')
+# print(os.getcwd())
 
 # os.mkdir('TEST')
 
-os.chdir('c:\\Users\\Yogi\\OneDrive\\Desktop\\TEST')
+# os.chdir('c:\\Users\\Yogi\\OneDrive\\Desktop\\TEST')
 # f=open('test.txt','w')
 # f.write('''
 # hi hello
@@ -55,5 +55,6 @@ os.chdir('c:\\Users\\Yogi\\OneDrive\\Desktop\\TEST')
 # os.removedirs('Grandparent/parent/child')
 
 # os.mkdir('TEST')
-os.rename('TEST','DEMO')
+
+# os.rename('TEST','DEMO')
 
