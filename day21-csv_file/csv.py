e1b01f9301ae0797170d5bdba55bdb6885d3b528
Yogendrareddyande <andeyogendra25@gmail.com>
2025-10-21 23:16:46 +0530

    updated

--- day21-csv_file/csv.py
+++ day21-csv_file/csv.py
@@ -1,23 +1,40 @@
 # CSV stands for Comma-Separated Values,
 
-# import csv
+import csv
 
-# import os
+import os
 
 
 
 # os.chdir('\\Users\\yogi\\OneDrive\\Desktop\\python')
 # print(os.getcwd())
 
-import csv
-import os
 
 # Always use the absolute path to the file
+# Read ()
+# import csv
 
+# with open("employees.csv", "r") as file:
+#     reader = csv.reader(file)
+#     for row in reader:
+#         print(row)
 
-with open('EMp', 'r') as f:
-    csv_reader = csv.reader(f)
-    for row in csv_reader:
-        print(row)
 
 # L,append
+
+'''
+with open("employees.csv", "w") as file:
+    file.write("ID,Name,Age,Department,Salary\n")
+    file.write("1,Yogi Reddy,23,IT,45000\n")
+    file.write("2,Anjali,25,HR,40000\n")
+    file.write("3,Ravi Kumar,28,Finance,52000\n")
+    file.write("4,Sneha,24,Marketing,48000\n")
+    file.write("5,Arjun,26,IT,50000\n")
+    file.write("6,Meena,27,Admin,43000\n")
+    file.write("7,Vikram,29,Sales,55000\n")
+    file.write("8,Kiran,30,Finance,60000\n")
+    file.write("9,Deepa,22,Support,41000\n")
+    file.write("10,Sravan,31,Manager,65000\n")
+
+print("✅ employees.csv file created successfully!")
+'''
